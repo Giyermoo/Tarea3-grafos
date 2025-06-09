c2b5790abb349cffac7668d1847fce956eb71aff
Guillermo Hidalgo <168038411+Giyermoo@users.noreply.github.com>
2025-06-09 18:51:32 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "modules/processor.h"
 #include "modules/validator.h"
 
 int main(int argc, char *argv[]) {
     if (!validar_argumentos(argc)) {
-        fprintf(stderr, "Uso: %s rutas.txt ORIGEN DESTINO\n", argv[0]);
+        fprintf(stderr, "Uso correcto: %s rutas.txt ORIGEN DESTINO\n", argv[0]);
         return 1;
     }
 
-    char *archivo = argv[1];
-    char *origen = argv[2];
-    char *destino = argv[3];
-
-    Grafo *grafo = crear_grafo_desde_archivo(archivo);
+    Grafo* grafo = crear_grafo_desde_archivo(argv[1]);
     if (!grafo) {
         fprintf(stderr, "Error al procesar el archivo.\n");
         return 1;
     }
 
-    encontrar_ruta_minima(grafo, origen, destino);
+    encontrar_ruta_minima(grafo, argv[2], argv[3]);
     liberar_grafo(grafo);
 
     return 0;
