2f203fe5e97eb11281ad83bd59e12889665b53a0
Guillermo Hidalgo <168038411+Giyermoo@users.noreply.github.com>
2025-06-09 18:46:31 -0400

    Update processor.h

---
+++ modules/processor.h
@@ -0,0 +1,24 @@
+#ifndef PROCESSOR_H
+#define PROCESSOR_H
+
+typedef struct Nodo {
+    char ciudad[4];
+    int peso;
+    struct Nodo* siguiente;
+} Nodo;
+
+typedef struct Vertice {
+    char ciudad[4];
+    Nodo* listaAdyacencia;
+    struct Vertice* siguiente;
+} Vertice;
+
+typedef struct {
+    Vertice* inicio;
+} Grafo;
+
+Grafo* crear_grafo_desde_archivo(const char* nombre_archivo);
+void encontrar_ruta_minima(Grafo* grafo, const char* origen, const char* destino);
+void liberar_grafo(Grafo* grafo);
+
+#endif
